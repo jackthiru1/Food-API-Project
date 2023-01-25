4194f2123c4567d59bdbe27e0e700c1881fb2586
jackson george <jacksongeorge@jacksons-MacBook-Pro.local>
2023-01-24 19:19:38 -0700

    styles added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ get_meal_btn.addEventListener('click', () => {
 	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
 		.then(res => res.json())
 		.then(res => {
-		createMeal(res.meals[0]);
+        createMeal(res.meals[0]);
 	});
 });
 
